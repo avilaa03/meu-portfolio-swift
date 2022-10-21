0b76596cde45fe77dc763f15fe27feb93e94b7d5
avilaa03 <lucasdeavila2003@gmail.com>
2022-10-21 01:37:42 -0300

    Atualização no código

---
+++ BackFront Academy/Arquiteturas/SwiftArquiteturas/SwiftArquiteturas/Infrastructure/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SwiftArquiteturas
+//
+//  Created by Lucas de Ávila Moreira on 21/10/22.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    struct ParametersKeys {
+        static let body = "body"
+        static let userModel = "userModel"
+    }
+}
